b1b248ba5f39df882ab66e836f87afb1fb6a59fc
Minzhe Zhang <zhang316421071@163.com>
2016-11-15 16:14:28 -0600

    first commit

---
+++ DIGRE_pipline.R
@@ -0,0 +1,97 @@
+###                    process_pipline.R                   ###
+### ====================================================== ###
+# This R script is a pipline to analyze drug pair synergy
+
+
+#######################################
+### Set working directory
+#######################################
+# User needs to change setwd() to their own directory that contains this script 
+setwd("/home2/s418336/project/drugCombination/CommandLineTool/")
+
+suppressMessages(library(argparse))
+
+
+#######################################
+### 0. Parse comandline argument
+#######################################
+
+### get arguments
+parser <- ArgumentParser(description = "This pipline is to analyze drug pair syngergy using DIGRE model.")
+parser$add_argument("geneExp", type = "character", help = "Passing in the gene expression data file name.")
+parser$add_argument("doseRes", type = "character", help = "Passing in the dose response data file name.")
+parser$add_argument("-p", "--pathway", type = "integer", default = 1, help =  "Specify the pathway information to use: 1 for consctructed KEGG pathway information, 2 de novo construction of gene network with own dataset. Default is 1.")
+parser$add_argument("-f", "--fold", type = "double", default = 0.6, help = "The gene expression fold change cut off to use, should be decimal between zero and one. Default is 0.6.")
+
+pip_args <- parser$parse_args()
+geneExp <- pip_args$geneExp
+doseRes <- pip_args$doseRes
+pathway <- pip_args$pathway
+fold <- pip_args$fold
+
+
+### concatenate input and output file path
+cur.path <- getwd()
+doseRes.path <- paste(cur.path, "/data/", doseRes, sep = "")
+geneExp.path <- paste(cur.path, "/data/", geneExp, sep = "")
+res.path <- paste(cur.path, "/report/", unlist(strsplit(geneExp, ".", fixed = TRUE))[1], ".scoreRank.csv", sep = "")
+
+
+
+
+
+#######################################
+### 1. Load library and functions
+#######################################
+cat("Loading packages and functions......\n")
+cat("------------\n")
+suppressMessages(library(preprocessCore))
+suppressMessages(library(org.Hs.eg.db))
+suppressMessages(library(KEGGgraph))
+
+source("code/doseRes.R")
+source("code/profileGeneExp.R")
+source("code/scoring.R")
+source("code/constGeneNet.R")
+load("data/CGP.mat.RData")
+load("data/KEGGnet.mat.RData")
+
+
+
+#######################################
+### 2. Read data
+#######################################
+
+# read dose response data
+doseRes <- readDoseRes.csv(doseRes.path)
+
+# prepare drug treated gene expression data
+geneExpDiff <- profileGeneExp(geneExp.path)
+
+# parse gene interaction data if uploaded
+if (pathway == 2) {
+      geneNet.mat <- constGeneNet(geneNet.path)
+}
+
+
+########################################
+### 3. Scoring by DIGRE model
+########################################
+cat("\nStart scoring all compound pairs by DIGRE model......\n")
+cat("------------\n")
+cat("Fold change cut off used: ", fold, "(default = 0.6)\n", sep = "")
+if (pathway == 1) {
+      cat("Pathway information used: KEGG pathway information\n")
+      res <- scoring(geneExpDiff = geneExpDiff, doseRes = doseRes, CGP.mat = CGP.mat, GP.mat = KEGGnet.mat, fold = fold)
+}
+if (pathway == 2) {
+      cat("Pathway information used: User specified gene network\n")
+      res <- scoring(geneExpDiff = geneExpDiff, doseRes = doseRes, CGP.mat = CGP.mat, GP.mat = geneNet.mat, fold = fold)
+}
+
+res.score <- res$scoreRank
+write.csv(res.score, res.path, row.names = FALSE)
+cat("\nScoring done, result file generated in report folder.\n")
+
+
+
---
+++ ReadMe.txt
@@ -0,0 +1,50 @@
+DIGRE_pipline.R
+---------------------------
+This R script is the pipline of DIGRE algorithm to analyze the drug pair synergistic score, using drug treated gene expression data and dose response curve data.
+
+
+Dependency
+----------
+*** R ***
+Following packages need to be installed:
+1. argparse
+	If you find problem in library this package, please try to install "proto" and "getopt" also.
+2. preprocessCore
+3. org.Hs.eg.db
+	If you find problem in library this package, please try to update your bioconductor using biocLite("BiocUpgrade").
+	You may also need try to install "RSQLite".
+4. KEGGgraph
+
+*** python 2 ***
+Following modules need to be installed:
+1. argparse 
+	This is build-in module in python 2.7. If you don't have python in your computer, I recommand to install anaconda, which include many useful modules.
+
+
+
+Run the pipeline
+----------------
+1. Change the working directory of the "DIGRE_pipline.R", which is to change the path of the setwd("...") to your own path that contains this file.
+2. Have the drug dose response data (csv file) in the data folder.
+3. Have the drug treated gene expression data (csv file) in the data folder.
+4. Have the pathway information "pathInfo.Rdata" and "gLassoGeneNet.Rdata" in the data folder.
+5. The result of the pipline would be generated in the report folder.
+
+Detail function see in code folder.
+Detail input data requirement see in the data folder.
+
+
+
+*** How to run the DIGRE pipline
+First change the setwd("") command in DIGRE_pipline.R script to the folder contains this file. 
+Then change the linux current directory to the folder contains this file.
+
+You need to specify four argument to run the R script [gene expression file name] followed by [dose response file name], [-p 1(or 2)] and [-f 0.6(or other cut off between 0 and 1)]
+-p: The pathway information to use: 1 for KEGG pathway information, 2 for constructed lymphoma gene network(partial corelation) information, 3 for constructed lymphoma gene network(marginal corelation) information. Default is 1.
+-f: The gene expression fold change cut off to use, should be decimal between zero and one. Default is 0.6.
+
+To run the demofile, just type:
+$Rscript DIGRE_pipline.R demoGeneExpr.csv demoDoseRes.csv  -p 1 -f 0.6
+
+For more information, just type:
+$Rscript DIGRE_pipline.R --help
